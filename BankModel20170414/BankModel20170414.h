ce03260f2d70229e0e7d879e71db00c046e1b7a7
Tennant Shaw <tennantshaw@macs-mbp.lan>
2017-04-15 11:50:42 -0400

    Initial Commit

---
+++ BankModel20170414/BankModel20170414.h
@@ -0,0 +1,19 @@
+//
+//  BankModel20170414.h
+//  BankModel20170414
+//
+//  Created by Tennant Shaw on 4/15/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BankModel20170414.
+FOUNDATION_EXPORT double BankModel20170414VersionNumber;
+
+//! Project version string for BankModel20170414.
+FOUNDATION_EXPORT const unsigned char BankModel20170414VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BankModel20170414/PublicHeader.h>
+
+
